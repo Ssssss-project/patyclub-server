cfa1b64582dac4f892e92334b82a633d3cf85257
ChiMi <charles01270@gmail.com>
2021-09-04 13:45:20 +0800

    add some test data

--- user.sql
+++ user.sql
@@ -14,3 +14,10 @@ TABLESPACE pg_default;
 
 ALTER TABLE public."User"
     OWNER to postgres;
+
+
+INSERT INTO public."User" (id, name, phone, remark) VALUES (0, 'John', '07-3325-4125', '')
+INSERT INTO public."User" (id, name, phone, remark) VALUES (1, 'Jenny', '085-331-621', '')
+INSERT INTO public."User" (id, name, phone, remark) VALUES (2, 'Charles', '0800-092-000', '')
+INSERT INTO public."User" (id, name, phone, remark) VALUES (3, 'Jean', '0800-000-123', '')
+INSERT INTO public."User" (id, name, phone, remark) VALUES (4, 'Pong', '02-3939-889', '')
