78167a2705f45a5f00031b2faa68935559514873
charles01270 <charles01270@gmail.com>
2021-11-09 13:42:54 +0800

    add MailKit add test sendMail(non-complete)

--- patyclub-server/Services/ForTestService.cs
+++ patyclub-server/Services/ForTestService.cs
@@ -1,3 +1,8 @@
+using MailKit.Net.Smtp;
+using MailKit;
+using MailKit.Security;
+using MimeKit;
+
 namespace patyclub_server.Service
 {
     public class TestService
@@ -5,6 +10,34 @@ namespace patyclub_server.Service
         public string concateTTT(string a, string b)
         {
             return a + b;
+        }
+
+        public void sendMail()
+        {
+            var message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress("PTC SERVER", "patyclub9453@gmail.com"));
+            message.To.Add(new MailboxAddress("TEST CLIENT", "charles01270@gmail.com"));
+
+            message.Subject = "How you doing?";
+            message.Body = new TextPart ("plain") {
+				Text = @"Hey Chandler,
+
+                    I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+                    -- Joey"
+			};
+
+
+			using (var client = new SmtpClient ()) {
+				client.Connect ("smtp.gmail.com", 465, true);
+
+				// Note: only needed if the SMTP server requires authentication
+				client.Authenticate ("patyclub9453@gmail.com", "paty-9748");
+
+				client.Send (message);
+				client.Disconnect (true);
+			}
         }
     }
     
